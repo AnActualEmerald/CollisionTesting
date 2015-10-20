5bd4d1f455c7568ba10042f7b7bff7ad33cabe8a
Burrito119 <kevinliger13@gmail.com>
2015-10-20 17:26:49 -0400

    Initil Commit

---
+++ CollisionTesting/PhysBody.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace CollisionTesting
+{
+    class PhysBody
+    {
+        private PointF verts;
+        private int _type;
+
+        public PhysBody(int body_type, float width , float height)
+        {
+            _type = body_type;
+            
+        }
+
+        private void _init_constructor()
+        {
+
+        }
+
+
+        public void Init()
+        {
+
+        }
+
+
+    }
+
+    class BodyFactory
+    {
+        public static readonly int RECT = 1;
+        public static readonly int CIRCLE = 2;
+        public static readonly int POLYGON = 3;
+
+        public static PointF[] MakeRectBody(float width, float height)
+        {
+            List<PointF> verts = new List<PointF>();
+
+            verts.Add(new PointF(0, 0));
+            verts.Add(new PointF(width, 0));
+            verts.Add(new PointF(width, height));
+            verts.Add(new PointF(0, height));
+
+            return verts.ToArray();
+        }
+
+
+
+    }
+    
+}
---
+++ CollisionTesting/PhysWorld.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollisionTesting
+{
+    class PhysWorld
+    {
+        #region variables
+        private List<PhysBody> bodies = new List<PhysBody>();
+        #endregion
+
+        public PhysWorld()
+        {
+
+        }
+
+    }
+}
